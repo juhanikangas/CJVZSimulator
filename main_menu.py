9abf1bfc1103bf77707dcdc09e615c18e7d43db5
LuukasLaitinen23 <luukal@metropolia.fi>
2023-09-20 11:19:56 +0300

    Fixed start game showing

--- main_menu.py
+++ main_menu.py
@@ -5,24 +5,35 @@ from select_flight_menu import choose_flight
 
 def main_menu(flight_specs):
     while True:
+        plane_selected = bool("user_plane" in flight_specs)
+        flight_selected = bool("departure_airport_name" in flight_specs and "destination_airport_name" in flight_specs)
+        plane_and_flight_selected = bool("user_plane" in flight_specs and "departure_airport_name" in flight_specs and "destination_airport_name" in flight_specs)
         print("Main Menu")
-        if "user_plane" in flight_specs:
+
+        if plane_selected:
             print(Fore.GREEN + "[1] " + Fore.RESET + "Change airplane")
         else:
             print(Fore.GREEN + "[1] " + Fore.RESET + "Choose airplane")
-        if "departure_airport_name" in flight_specs and "destination_airport_name" in flight_specs:
+
+        if flight_selected:
             print(Fore.GREEN + "[2] " + Fore.RESET + "Change flight")
         else:
             print(Fore.GREEN + "[2] " + Fore.RESET + "Choose flight")
-        print(Fore.GREEN + "[3] " + Fore.RESET + "Start game")
+
+        if plane_and_flight_selected:
+            print(Fore.GREEN + "[3] " + Fore.RESET + "Start game")
+
         action = input("Select: ")
+
         if action == "1":
             flight_specs = choose_plane(flight_specs)
         elif action == "2":
             flight_specs = choose_flight(flight_specs)
         elif action == "3":
-            # Must have flight and airplane selected to be able to start game
-            print("Game started")
-            break
+            if plane_and_flight_selected:
+                print("Game started")
+                break
+            else:
+                print("Invalid input")
         else:
             print("Invalid input")
